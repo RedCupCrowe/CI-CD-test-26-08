15e7262b1237a90de6defe9d245ebd2f552ebd0a
RedCupCrowe <valdemarkjohansen@gmail.com>
2022-08-26 09:47:34 +0200

    test af template og azure samtidig

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
   @GetMapping("/firstMapping")
   public String firstMapping(){
-    return "Hello World";}
+    return "testAfTemplate";}
   @GetMapping("/")
   public String rootMapping(){
     return "Default Page!";}
